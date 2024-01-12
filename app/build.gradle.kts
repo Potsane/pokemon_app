139230e080c9e8a7484117c2406962e9100a9a86
Potsane Mohale <potsane@luno.com>
2024-01-12 19:23:11 +0200

    add hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+
+    /*alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlinKsp)
+    alias(libs.plugins.kotlinSerialization)*/
 }
 
 android {
@@ -60,11 +64,16 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+
+    implementation(libs.hilt)
+//    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(platform(libs.compose.bom))
     androidTestImplementation(libs.ui.test.junit4)
+
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
 }
